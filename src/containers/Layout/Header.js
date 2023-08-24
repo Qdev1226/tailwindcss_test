24e57ff134b44206ff7f45ed14f8e19d2e893b86
Jay <wmeng0103@gmail.com>
2023-08-24 10:49:28 +1000

    regular

--- src/containers/Layout/Header.js
+++ src/containers/Layout/Header.js
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
 
-const Header = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const handleDropdownItemClick = () => {
-    setDropdownOpen(false);
-  };
+function Header() {
+  const [dropdownOpen1, setDropdownOpen1] = useState(false);
+  const [dropdownOpen2, setDropdownOpen2] = useState(false);
+  const [dropdownOpen3, setDropdownOpen3] = useState(false);
+  const onButtonClick = (e) => {};
   return (
-    <header className="p-4 bg-red-800 text-white">
+    <header className="p-4 bg-blue-500 text-white">
       <div className="container mx-auto flex justify-between">
-        <div>Home</div>
-        <div className="relative">
-          <button onClick={() => setDropdownOpen(!dropdownOpen)} className="focus:outline-none">
-            {/* <svg className="h-5 w-5 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            </svg> */}
-            Settings
-          </button>
-          {dropdownOpen && (
-            <ul className="border py-2 absolute mt-1 w-32 bg-white text-black rounded shadow-lg">
-              <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={handleDropdownItemClick}>Menu 1</li>
-              <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={handleDropdownItemClick}>Menu 2</li>
-              <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={handleDropdownItemClick}>Menu 3</li>
-            </ul>
-          )}
+        <div>Logo</div>
+        <div className="flex space-x-4">
+          <div className="relative" onClick={() => setDropdownOpen1(!dropdownOpen1)}>
+            <button className="focus:outline-none">Menu1</button>
+            {dropdownOpen1 && (
+              <ul className="border py-2 absolute mt-1 w-32 bg-white text-black rounded shadow-lg">
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen1(false)}>Item 1</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen1(false)}>Item 2</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen1(false)}>Item 3</li>
+              </ul>
+            )}
+          </div>
+          <div className="relative" onClick={() => setDropdownOpen2(!dropdownOpen2)}>
+            <button className="focus:outline-none">Menu2</button>
+            {dropdownOpen2 && (
+              <ul className="border py-2 absolute mt-1 w-32 bg-white text-black rounded shadow-lg">
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen2(false)}>Item 1</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen2(false)}>Item 2</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen2(false)}>Item 3</li>
+              </ul>
+            )}
+          </div>
+          <div className="relative" onClick={() => setDropdownOpen3(!dropdownOpen3)}>
+            <button className="focus:outline-none">Menu3</button>
+            {dropdownOpen3 && (
+              <ul className="border py-2 absolute mt-1 w-32 bg-white text-black rounded shadow-lg">
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen3(false)}>Item 1</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen3(false)}>Item 2</li>
+                <li className="px-4 py-2 hover:bg-blue-100 transition-colors duration-150" onClick={() => setDropdownOpen3(false)}>Item 3</li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </header>
